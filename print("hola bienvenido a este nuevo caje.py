f88760b9347360c1fd15f245eac46678cb8067bd
paco4848 <nicolasmolinero587@gmail.com>
2026-05-20 23:36:36 +0000

    cambio de idea

--- print("hola bienvenido a este nuevo caje.py
+++ print("hola bienvenido a este nuevo caje.py
@@ -1,2 +1,9 @@
-print("hola bienvenido a este nuevo cajero")
-
+while True:
+    pregunta = input("hola quieres lanzar el dado? si o no?")
+    if pregunta == "si":
+        resultado = random.randint(1, 6)
+        print("el resultado del dado es: ", resultado)
+     elif pregunta == "no":
+        print("bueno, hhasta luego")
+        break
+    else: print("pon que si para poder jugar")
