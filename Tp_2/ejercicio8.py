8d4801fc2c88515d9b071544d814b8437fea6698
paco4848 <nicolasmolinero587@gmail.com>
2026-08-05 22:06:51 +0000

    a

--- Tp_2/ejercicio8.py
+++ Tp_2/ejercicio8.py
@@ -2,7 +2,6 @@ import random
 palabra = ["Sol", "camino", "libro", "ventana", "estrella", "Musica", "Jardin", "playa", "Tiempo", "aventura"]
 palabra_seleccionada = random.choice(palabra).lower()
 
-print(palabra_seleccionada)
 ocultar = len(palabra_seleccionada) * ["_"]
 for n in range(6):
     letras_intentadas = []
